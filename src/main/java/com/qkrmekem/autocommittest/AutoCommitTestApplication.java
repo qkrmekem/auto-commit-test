a1d084f63c2e0bd667152b4e60433ae3e3293237
qkrmekem <magicofclown@naver.com>
2023-07-15 06:20:57 +0900

    first commit

---
+++ src/main/java/com/qkrmekem/autocommittest/AutoCommitTestApplication.java
@@ -0,0 +1,13 @@
+package com.qkrmekem.autocommittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoCommitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoCommitTestApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/qkrmekem/autocommittest/AutoPushTest.java
@@ -0,0 +1,52 @@
+package com.qkrmekem.autocommittest;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.RemoteAddCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.transport.URIish;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.springframework.stereotype.Repository;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class AutoPushTest {
+    public static void main(String[] args) throws IOException, GitAPIException {
+        try (Git git = Git.open(new File("C:\\Users\\10\\Desktop\\algorithm\\auto-commit"))) {
+            // Add your logic here ...
+            RemoteAddCommand remoteAddCommand = git.remoteAdd();
+            remoteAddCommand.setName("origin");
+            try {
+                remoteAddCommand.setUri(new
+                        URIish("https://github.com/qkrmekem/auto-commit.git"));
+                System.out.println("file Added");
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+
+            git.add().addFilepattern(".").call();
+            // you can add more settings here if needed
+            remoteAddCommand.call();
+            git.commit().setMessage( "commited" ).call();
+
+            // push to remote:
+            PushCommand pushCommand = git.push();
+            pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider("qkrmekem", "pem89als@@"));
+            // you can add more settings here if needed
+            pushCommand.call();
+
+            try {
+                git.push().setRemote("origin").add("master").call();
+            } catch (TransportException e) {
+                // Add your own logic here, for example:
+                System.out.println("Username or password incorrect.");
+            }
+
+        }
+
+    }
+}
